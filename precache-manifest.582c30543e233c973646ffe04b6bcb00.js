da6f7efbbd1aa4739f83e3bbbc05fdd88e3f7094
AlfredoCU <alfredo_11_cu@hotmail.com>
2020-07-03 19:36:30 -0500

    Updates

---
+++ precache-manifest.582c30543e233c973646ffe04b6bcb00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7112ad3b2cc9f717dd7d06b0e15109",
+    "url": "/API-GIPHT/index.html"
+  },
+  {
+    "revision": "8aacd227c83ca15f49a9",
+    "url": "/API-GIPHT/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "390bdd0172a2178e58ff",
+    "url": "/API-GIPHT/static/js/2.efc32590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/API-GIPHT/static/js/2.efc32590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aacd227c83ca15f49a9",
+    "url": "/API-GIPHT/static/js/main.798ad56e.chunk.js"
+  },
+  {
+    "revision": "237648a8f3d02589afdb",
+    "url": "/API-GIPHT/static/js/runtime-main.82ace36c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/API-GIPHT/static/media/logo.5d5d9eef.svg"
+  }
+]);
